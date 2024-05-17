e939a50a914f2deaed8872597f7cc1f583fe4ad6
Kota Mizushima <kmizu.main@gmail.com>
2024-05-17 22:32:15 +0900

    抽象構文木ヴィジュアライザの改善

--- src/main/scala/com/github/nuko/Visualizer.scala
+++ src/main/scala/com/github/nuko/Visualizer.scala
@@ -2,7 +2,10 @@ package com.github.nuko
 
 object Visualizer {
   def main(args: Array[String]): Unit = {
-    val ast = Ast.BinaryExpression(NoLocation, Operator.ADD, Ast.IntNode(NoLocation, 1), Ast.IntNode(NoLocation, 2))
+    val ast = Ast.BinaryExpression(NoLocation, Operator.SUBTRACT,
+      Ast.BinaryExpression(NoLocation, Operator.ADD, Ast.IntNode(NoLocation, 1), Ast.IntNode(NoLocation, 2)),
+      Ast.BinaryExpression(NoLocation, Operator.ADD, Ast.IntNode(NoLocation, 3), Ast.IntNode(NoLocation, 4)),
+    )
     ASTVisualizer.visualize(ast)
   }
 }
