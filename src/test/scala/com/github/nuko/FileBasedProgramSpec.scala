d95ad5fc60c6a8bc45a4032f07cf132c6a0d1c52
Kota Mizushima <kmizu.main@gmail.com>
2024-06-12 23:12:32 +0900

    拡張子を.niから.nkに変更

--- src/test/scala/com/github/nuko/FileBasedProgramSpec.scala
+++ src/test/scala/com/github/nuko/FileBasedProgramSpec.scala
@@ -7,7 +7,7 @@ import com.github.scaruby.SFile
 class FileBasedProgramSpec extends SpecHelper {
   val directory = new SFile("test-programs")
   describe(s"run Nihongo programs under ${directory}") {
-    for(program <- directory.listFiles{file => file.name.endsWith(".ni")}) {
+    for(program <- directory.listFiles{file => file.name.endsWith(".nk")}) {
       it(s"program ${program} runs successfully") {
         try {
           E.evaluateFile(program)
